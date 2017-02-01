243f608d80134f3338eb0b00a8355643555e94c1
MARC LOOS <marc.loos@nl.ibm.com>
2017-02-01 09:26:39 -0600

    test 1234

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content = "text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
